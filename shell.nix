218f83489ab44c6f191b91ffe42f25fd9227ca57
NWS99 <warmuthniklas@googlemail.com>
2023-12-25 20:23:02 +0100

    try out slicing of lists

--- shell.nix
+++ shell.nix
@@ -1,9 +1,8 @@
 { pkgs ? import <nixpkgs> {} }:
 let
   python-packages = ps: with ps; [
-      jupyter
-      ipython
       pip
+      numpy
   ];
   my-python = pkgs.python310.withPackages python-packages;
 in my-python.env
